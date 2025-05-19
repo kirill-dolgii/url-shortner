0e7631d2d14a48c0ab470ee1feafd99815ab7a00
Kirill Dolgii <maindolgiykirill99@gmail.com>
2025-05-19 20:01:50 +0300

    -

--- internal/storage/storage.go
+++
@@ -1,9 +0,0 @@
-package storage
-
-import "errors"
-
-var (
-	ErrUrlExists     = errors.New("url exists")
-	ErrAliasNotFount = errors.New("alias not found")
-	ErrUrlNotFound   = errors.New("url not found")
-)
